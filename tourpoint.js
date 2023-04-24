e3fc5b972c790ef5b3c6c5201e25fdee16441599
Hijklmnopraew <124548211+Hijklmnopraew@users.noreply.github.com>
2023-04-24 10:49:18 +0700

    Add files via upload

---
+++ tourpoint.js
@@ -0,0 +1,155 @@
+var tourpoint = {
+    "type": "FeatureCollection",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "ทะเลแหวก",
+          "type": "์Natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.81122600000123,
+            7.9607482496605115
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อ่าวนาง",
+          "type": "Natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.83546100000041,
+            8.040166628770578
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะพีพี",
+          "type": "Natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.81558172883825,
+            8.03930349891182
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "วัดถ้ำเสือ",
+          "type": "Culture",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.92498450000073,
+            8.124310633644996
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "อ่าวไร่เลย์ ตะวันออก",
+          "type": "Natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.84147927116459,
+            8.010673627075207
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "สุสานหอย 75 ล้านปี",
+          "type": "Natural",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.88400400000171,
+            8.02256487542769
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "จุดชมวิว เขาหงอนนาค",
+          "type": "Natural",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.77614000000113,
+            8.086925875766141
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะปอดะ",
+          "type": "Natural",
+          "level": "⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.81198499999817,
+            7.974585875178647
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะลันตา",
+          "type": "Natural",
+          "level": "⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            99.03964799999795,
+            7.619531605165761
+          ],
+          "type": "Point"
+        }
+      },
+      {
+        "type": "Feature",
+        "properties": {
+          "name": "เกาะห้อง",
+          "type": "Natural",
+          "level": "⭐⭐⭐⭐⭐"
+        },
+        "geometry": {
+          "coordinates": [
+            98.67918550000024,
+            8.07879699808096
+          ],
+          "type": "Point"
+        }
+      }
+    ]
+  }
